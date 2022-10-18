a856c0523206caac6735a0f3a2dbfe9c8dda9edb
invalidcode232 <invalidcode69@gmail.com>
2022-10-18 15:46:42 +0700

    feat: add terminal binds

--- lua/binds.lua
+++ lua/binds.lua
@@ -15,4 +15,22 @@ keyset("n", "<A-h>", ":bprev<cr>")
 keyset("n", "<A-l>", ":bnext<cr>")
 keyset("n", "<A-x>", ":BufferClose<cr>")
 
+keyset("n", "<C-`>", ":ToggleTerm direction=vertical size=100<cr>")
+keyset("t", "<C-`>", "<C-\\><C-N>:ToggleTerm direction=vertical size=100<cr>")
+keyset("i", "<C-`>", "<C-\\><C-N>:ToggleTerm direction=vertical size=100<cr>")
+
+vim.cmd([[
+    :tnoremap <C-h> <C-\><C-N><C-w>h
+    :tnoremap <C-j> <C-\><C-N><C-w>j
+    :tnoremap <C-k> <C-\><C-N><C-w>k
+    :tnoremap <C-l> <C-\><C-N><C-w>l
+		:inoremap <C-h> <C-\><C-N><C-w>h
+    :inoremap <C-j> <C-\><C-N><C-w>j
+    :inoremap <C-k> <C-\><C-N><C-w>k
+    :inoremap <C-l> <C-\><C-N><C-w>l
+    :nnoremap <C-h> <C-w>h
+    :nnoremap <C-j> <C-w>j
+    :nnoremap <C-k> <C-w>k
+    :nnoremap <C-l> <C-w>l
+]])
 
--- lua/misc.lua
+++ lua/misc.lua
@@ -7,6 +7,10 @@ use {
 }
 
 use {
+	"akinsho/toggleterm.nvim"
+}
+
+use {
 	'dstein64/vim-startuptime'
 }
 
@@ -14,6 +18,15 @@ local wilder = require("wilder")
 wilder.setup({modes = {':', '?'}})
 
 require("leap").add_default_mappings()
+require("toggleterm").setup {
+	function (term)
+		    if term.direction == "horizontal" then
+      return 15
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 5
+    end
+	end
+}
 
 keyset("n", "<Space>/", "gcc")
 keyset("v", "<Space>/", "gc")
