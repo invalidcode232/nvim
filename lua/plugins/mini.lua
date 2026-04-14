fcbce5e34aaab0d74e6a9a59376c62de04b45008
James <james@Jamess-MacBook-Air-3.local>
2026-04-14 08:34:33 +0100

    fix: conflicting keybinds of flash.nvim and mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,7 +5,17 @@ return {
         -- Core editor
         require("mini.pairs").setup()
         require("mini.icons").setup()
-        require("mini.surround").setup()
+        require("mini.surround").setup({
+            mappings = {
+                add = "gza", -- Add surrounding in Normal and Visual modes
+                delete = "gzd", -- Delete surrounding
+                find = "gzf", -- Find surrounding (to the right)
+                find_left = "gzF", -- Find surrounding (to the left)
+                highlight = "gzh", -- Highlight surrounding
+                replace = "gzr", -- Replace surrounding
+                update_n_lines = "gzn", -- Update `n_lines`
+            },
+        })
         require("mini.comment").setup()
 
         -- Misc
