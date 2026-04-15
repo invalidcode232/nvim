f95e3b066c159ce6d9a11f4085217adab2c7cc0e
James <james@Jamess-MacBook-Air-3.local>
2026-04-15 13:39:47 +0100

    feat: enable LSPs for typescript and tailwind

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,33 +1,33 @@
 -- LSP related settings
 
 -- Enable the following LSPs
-local lsps = {"lua_ls", "pyright", "clangd"}
+local lsps = { "lua_ls", "pyright", "clangd", "ts_ls", "tailwindcss" }
 vim.lsp.enable(lsps)
 
 -- Use icons as signs
 local signs = {
-    Error = " ",
-    Warn = " ",
-    Hint = "󰌵 ",
-    Info = " "
+	Error = " ",
+	Warn = " ",
+	Hint = "󰌵 ",
+	Info = " ",
 }
 
 local signConf = {
-  text = {},
-  texthl = {},
-  numhl = {},
+	text = {},
+	texthl = {},
+	numhl = {},
 }
 
 for type, icon in pairs(signs) do
-  local severityName = string.upper(type)
-  local severity = vim.diagnostic.severity[severityName]
-  local hl = "DiagnosticSign" .. type
-  signConf.text[severity] = icon
-  signConf.texthl[severity] = hl
-  signConf.numhl[severity] = hl
+	local severityName = string.upper(type)
+	local severity = vim.diagnostic.severity[severityName]
+	local hl = "DiagnosticSign" .. type
+	signConf.text[severity] = icon
+	signConf.texthl[severity] = hl
+	signConf.numhl[severity] = hl
 end
 
 vim.diagnostic.config({
-  signs = signConf,
-  virtual_text = true
+	signs = signConf,
+	virtual_text = true,
 })
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,28 +1,28 @@
 return {
-    'nvim-mini/mini.nvim',
-    version = false,
-    config = function ()
-        -- Core editor
-        require("mini.pairs").setup()
-        require("mini.icons").setup()
-        require("mini.surround").setup({
-            mappings = {
-                add = "gza", -- Add surrounding in Normal and Visual modes
-                delete = "gzd", -- Delete surrounding
-                find = "gzf", -- Find surrounding (to the right)
-                find_left = "gzF", -- Find surrounding (to the left)
-                highlight = "gzh", -- Highlight surrounding
-                replace = "gzr", -- Replace surrounding
-                update_n_lines = "gzn", -- Update `n_lines`
-            },
-        })
-        require("mini.comment").setup()
+	"nvim-mini/mini.nvim",
+	version = false,
+	config = function()
+		-- Core editor
+		require("mini.pairs").setup()
+		require("mini.icons").setup()
+		require("mini.surround").setup({
+			mappings = {
+				add = "gza", -- Add surrounding in Normal and Visual modes
+				delete = "gzd", -- Delete surrounding
+				find = "gzf", -- Find surrounding (to the right)
+				find_left = "gzF", -- Find surrounding (to the left)
+				highlight = "gzh", -- Highlight surrounding
+				replace = "gzr", -- Replace surrounding
+				update_n_lines = "gzn", -- Update `n_lines`
+			},
+		})
+		require("mini.comment").setup()
 
-        -- Misc
-        -- Cmdline autocomplete and improvements
-        require("mini.cmdline").setup()
+		-- Misc
+		-- Cmdline autocomplete and improvements
+		require("mini.cmdline").setup()
 
-        -- Nicer statusline
-        require("mini.statusline").setup()
-    end
+		-- Nicer statusline
+		require("mini.statusline").setup()
+	end,
 }
